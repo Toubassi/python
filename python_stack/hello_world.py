de0df934aa756e8a26efa127b73093c780ab6a77
Toubassi <emil_toubassi@hotmail.com>
2026-09-24 13:18:13 +0300

    python_stack

---
+++ python_stack/hello_world.py
@@ -0,0 +1,14 @@
+print('Hello World')
+first_name = "Emil"
+
+print('Hello', str(first_name))
+print('Hello ' + str(first_name))
+
+num = 4
+print('Hello', str(num))
+print('Hello ' + str(num))
+
+fav_food1 = "Pizza"
+fav_food2 = "Burger"
+print(f"I love to eat {fav_food1} and {fav_food2}")
+
